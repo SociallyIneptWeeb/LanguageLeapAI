78aa5e5ba20e1211d0caa33920835eec1231b658
SociallyIneptWeeb <sociallyinept.csit@gmail.com>
2023-03-12 12:31:37 +0800

    refactor: small changes

--- src/modules/voicevox.py
+++ src/modules/voicevox.py
@@ -31,11 +31,14 @@ VOICEVOX_WAV_PATH = Path(__file__).resolve().parent.parent / r'audio\voicevox.wa
 
 def play_voice(device_id):
     data, fs = sf.read(VOICEVOX_WAV_PATH, dtype='float32')
-    if (INGAME_PUSH_TO_TALK_KEY != '') :
+
+    if INGAME_PUSH_TO_TALK_KEY:
         keyboard.press(INGAME_PUSH_TO_TALK_KEY)
+
     sd.play(data, fs, device=device_id)
     sd.wait()
-    if (INGAME_PUSH_TO_TALK_KEY != '') :
+
+    if INGAME_PUSH_TO_TALK_KEY:
         keyboard.release(INGAME_PUSH_TO_TALK_KEY)
 
 
